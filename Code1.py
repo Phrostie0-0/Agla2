8b22a2a53116c9b75fcce8207d739a370a9ac164
Ruslan <ru.nasibullin@innopolis.university>
2025-01-27 23:43:11 +0300

    Update Code1.py
    I added exception

--- Code1.py
+++ Code1.py
@@ -8,7 +8,13 @@ for i in range(N):
     row = list(map(float, input(f"Line {i + 1}: ").split()))
     m1.append(row)
 m2 = sp.Matrix(m1)
-rref, pivots = m2.rref()
-print(f"Solution for system with {N} variables:")
-for i in pivots:
-    print(round(rref[i, N], 3), end=" ")
+try:
+    rref, pivots = m2.rref()
+    for i in pivots:
+        rref[i, N] = rref[i, N]
+except IndexError:
+    print("There is no solution for that system")
+else:
+    print(f"Solution for system with {N} variables:")
+    for i in pivots:
+        print(round(rref[i, N], 3), end=" ")
